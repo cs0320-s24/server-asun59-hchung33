feae985536fe1f3be4b3d1a3420913570d6d8191
Hee Su Chung <jhschung88@gmail.com>
2024-02-11 17:03:54 -0500

    feat: loadCSV includes file path parsing

--- src/main/java/edu/brown/cs/student/main/server/loadCSV.java
+++ src/main/java/edu/brown/cs/student/main/server/loadCSV.java
@@ -1,12 +1,25 @@
 package edu.brown.cs.student.main.server;
 
+import edu.brown.cs.student.main.creator.StringCreator;
 import edu.brown.cs.student.main.parser.CSVParser;
+import edu.brown.cs.student.main.parser.CreatorFromRow;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
 import java.util.List;
 
 public class loadCSV {
     public loadCSV(String path){
-        CSVParser<List<String>> parser = new CSVParser<List<String>>();
+        try {
+            Reader fReader = new FileReader(path);
+            CreatorFromRow<List<String>> creator = new StringCreator();
+            CSVParser<List<String>> parser = new CSVParser<List<String>>(fReader, creator);
+
+        } catch (FileNotFoundException e){
+            System.err.println("FileNotFound");
+        }
+
 
     }
 
