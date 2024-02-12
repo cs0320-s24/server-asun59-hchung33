ed73b37724f8689e0c757d83178a643edc5fbc7c
Alyssa2026 <Alyssasun2022@gmail.com>
2024-02-12 10:29:17 -0500

    feat: starting search handler

--- src/main/java/edu/brown/cs/student/main/handler/SearchCSVHandler.java
+++ src/main/java/edu/brown/cs/student/main/handler/SearchCSVHandler.java
@@ -4,6 +4,7 @@ import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 import com.squareup.moshi.Types;
 import edu.brown.cs.student.main.datasource.ParseDatasource;
+import edu.brown.cs.student.main.parser.CSVSearcher;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,17 +32,28 @@ public class SearchCSVHandler implements Route {
 
     try {
       String path = request.queryParams("path");
+      // TODO: get user input based on search
+      String toSearch = request.queryParams("toSearch");
+      String headerPresent = request.queryParams("headerPresent");
+      String columnIsNum = request.queryParams("columnIsNum");
+      String columnIDInteger = request.queryParams("columnIDInteger"); // only one header in list
+      String columnIDString = request.queryParams("columnIDString"); // only one header will be in list
+
       if (this.state.getMap().containsKey(path)) {
         // Get the parsed data
         List<List<String>> parsedData = this.state.getParsed();
-        // take in url input and search
+        // TODO: change search to jsut take in parsed data
+        // TODO: search based on input
+        // TODO: use most to print out output
+
+
 
       }
     } catch (Exception e) {
       errorJson.put("error", e.getMessage());
       return adapterError.toJson(errorJson);
     }
-    errorJson.put("error", "File can not be viewed");
+    errorJson.put("error", "File can not be searched");
     return adapterError.toJson(errorJson);
   }
 }
