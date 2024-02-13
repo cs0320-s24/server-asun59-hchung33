e21bed9d0cb3f4cceb690898bff8ed359e2a7748
Alyssa2026 <Alyssasun2022@gmail.com>
2024-02-12 22:52:15 -0500

    feat: added to broadbandhandler

--- src/main/java/edu/brown/cs/student/main/handler/BroadbandHandler.java
+++ src/main/java/edu/brown/cs/student/main/handler/BroadbandHandler.java
@@ -4,6 +4,8 @@ package edu.brown.cs.student.main.handler;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 import com.squareup.moshi.Types;
+import edu.brown.cs.student.main.datasource.BroadbandDatasource;
+import edu.brown.cs.student.main.datasource.BroadbandDatasource.StateResponse;
 import edu.brown.cs.student.main.datasource.ParseDatasource;
 
 import java.lang.reflect.Type;
@@ -16,8 +18,9 @@ import spark.Route;
 
 public class BroadbandHandler implements Route {
     private final static String API_Key= "808169d08601aed7dba214b43be6999b1909e403";
-    public BroadbandHandler(){
-
+    private final BroadbandDatasource state;
+    public BroadbandHandler(BroadbandDatasource state){
+        this.state= state;
     }
 
     @Override
@@ -31,19 +34,30 @@ public class BroadbandHandler implements Route {
                                 Map.class, String.class, String.class)); // ERROR HANDLING
 
         Moshi moshiReturn = new Moshi.Builder().build();
-        Map<String, String> returnJson = new HashMap<>();
+        Map<String, String> responseMap = new HashMap<>();
         JsonAdapter<Map<String, String>> adapterReturn =
                 moshiReturn.adapter(Types.newParameterizedType(Map.class, String.class, String.class));
+
+        String state = request.queryParams("state");
+        String year = request.queryParams("year");
+        String county = request.queryParams("county");
+
+        String variable = "S2802_C03_022E";
+
+        if(state == null || year == null || county==null) {
+            // Bad request! Send an error response.
+            responseMap.put("error_type", "missing_parameter");
+            return adapterError.toJson(responseMap);
+        }
+        // get census data
         try {
-            String state = request.queryParams("state");
-            String year = request.queryParams("year");
-            String county = request.queryParams("county");
-
-            String variable = "S2802_C03_022E";
-
-            // TODO: convert census numerical value
-            // Gear up +live code
-
+            // get state ID
+            StateResponse stateID = this.state.getStateIDPublic();
+            // Get all data
+            int yearInt = Integer.parseInt(year);
+//          StateResponse wifiData = this.state.getStateIDPublic();
+//          responseMap.put("type", "success");
+//          return adapterReturn.toJson(responseMap);
         } catch (Exception e) {
             errorJson.put("error", e.getMessage());
             return adapterError.toJson(errorJson);
