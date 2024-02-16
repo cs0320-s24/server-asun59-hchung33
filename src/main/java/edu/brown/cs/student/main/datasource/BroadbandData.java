152de322064e6d1d8b883b649448ea7c98f7cb5e
Hee Su Chung <jhschung88@gmail.com>
2024-02-16 17:30:41 -0500

    style: adjusting

--- src/main/java/edu/brown/cs/student/main/datasource/BroadbandData.java
+++ src/main/java/edu/brown/cs/student/main/datasource/BroadbandData.java
@@ -2,7 +2,6 @@ package edu.brown.cs.student.main.datasource;
 
 /**
  * This is a record class that stores the broadband percentage of a county.
- *
  * @param data
  */
 public record BroadbandData(String data) {}
