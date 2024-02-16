1d02b0e37e8a39462a2a4a305e57c42d4f53f83e
Alyssa2026 <Alyssasun2022@gmail.com>
2024-02-16 17:29:09 -0500

    style: added mock comments

--- src/test/java/edu/brown/cs/student/Mock/MockBroadbandDatasource.java
+++ src/test/java/edu/brown/cs/student/Mock/MockBroadbandDatasource.java
@@ -7,35 +7,62 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+/**
+ * This class implements the interface. It allows us to get mock data and prevents us from needing
+ * to call the api many times for testing
+ */
+
 public class MockBroadbandDatasource implements BroadbandInterface {
   private String data;
+
+  /**
+   * Mock class constructor
+   * @param data
+   */
 
   public MockBroadbandDatasource(String data) {
     this.data = data;
   }
 
+  /**
+   * Override method that returns the data for mock data
+   * @param cache CacheBroadbandDatasource that stores recent queries or performs expensive
+   *     computation
+   * @param stateID ID of the state being queried
+   * @param countyID ID of the county being queried
+   * @return
+   * @throws ExecutionException
+   */
   @Override
   public BroadbandData broadbandDataProxy(
       CacheBroadbandDatasource cache, String stateID, String countyID) throws ExecutionException {
     return new BroadbandData(this.data);
   }
 
+  /**
+   * Override method for interface
+   * @return
+   */
   @Override
   public List<List<String>> getStatesIDs() {
     return new ArrayList<List<String>>();
   }
 
+  /**
+   * Override method for interface
+   * @return
+   */
   @Override
   public List<List<String>> getCountyIDs() {
     return new ArrayList<List<String>>();
   }
-
+  /**
+   * Override method for interface
+   * @return
+   */
   @Override
   public BroadbandData getInternetData(String stateID, String countyID) {
     return new BroadbandData(this.data);
   }
 
-  public void setNUll() {
-    this.data = null;
-  }
 }
