ce6772d7e4f4566bb9b31413fb9a247b96c36d74
Hee Su Chung <jhschung88@gmail.com>
2024-02-16 17:29:28 -0500

    docs: Server comments

--- src/main/java/edu/brown/cs/student/main/server/Server.java
+++ src/main/java/edu/brown/cs/student/main/server/Server.java
@@ -10,9 +10,18 @@ import edu.brown.cs.student.main.handler.SearchCSVHandler;
 import edu.brown.cs.student.main.handler.ViewCSVHandler;
 import spark.Spark;
 
+/**
+ * This is the main class for Server.
+ */
 public class Server {
   static final int port = 5300;
 
+  /**
+   * This is the constructor for the server. This runs the port, and sets up each Handler to
+   * receive user input at each endpoint. Following endpoints are set here: LoadCSVHandler,
+   * ViewCSVHandler, SearchCSVHandler, and BroadbandHandler. Afterward, Spark waits for
+   * the server.
+   */
   public Server() {
     Spark.port(port);
     after(
@@ -39,6 +48,11 @@ public class Server {
     Spark.awaitInitialization();
   }
 
+  /**
+   * This is main method for the program. It constructs the Server and prints an informative
+   * message indicating that server has started at port number above.
+   * @param args Arguments for the Server
+   */
   public static void main(String[] args) {
     new Server();
     System.out.println("Server started at http://localhost:" + port);
